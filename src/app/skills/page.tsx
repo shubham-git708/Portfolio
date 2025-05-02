e00dd259b2379e59089993c8caf180ebc2e8f583
Shubham Prasad <shubhamprasad@Shubhams-MacBook-Pro.local>
2025-05-02 23:03:45 +0530

    feat: align Analytics Tools beside Tech Stack and Soft Skills, and move main heading below navbar in Skills tab

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -12,6 +12,10 @@ const skillPage = () => {
       <div className="absolute inset-0 bg-[radial-gradient(circle,_var(--primary-sky)_1px,_transparent_1px)] bg-[size:20px_20px] opacity-10 z-0 pointer-events-none" />
       <div className="flex flex-col gap-3 w-full relative z-10">
         <div className="relative z-10">
+          <Badge variant="secondary" className="gap-1.5 py-1 ">
+            <LightbulbIcon className="w-4 h-4" />
+            My Skills
+          </Badge>
           <Heading>My Technical Experience/Skills.</Heading>
           <FramerWrapper y={0} x={200}>
             <p className="font-poppins text-xl w-full text-primary max-sm:text-lg">
@@ -38,6 +42,16 @@ const skillPage = () => {
               </FramerWrapper>
             </div>
             <div className="flex-1">
+              <FramerWrapper y={100} delay={0.32} className="block w-full">
+                <h1 className="gap-2 text-2xl font-poppins text-primary font-semibold flex text_underline relative max-sm:text-xl mb-4">
+                  Analytics Tools
+                </h1>
+                <div className="w-full flex flex-wrap gap-4">
+                  <SkillsFooter items={portfolioConfig.skills.analyticsTools} />
+                </div>
+              </FramerWrapper>
+            </div>
+            <div className="flex-1">
               <FramerWrapper y={100} delay={0.33} className="block w-full">
                 <h1 className="gap-2 text-2xl font-poppins text-primary font-semibold flex text_underline relative max-sm:text-xl mb-4">
                   Soft Skills
@@ -48,14 +62,6 @@ const skillPage = () => {
               </FramerWrapper>
             </div>
           </div>
-          <FramerWrapper y={100} delay={0.32} className="block w-full">
-            <h1 className="gap-2 text-2xl font-poppins text-primary font-semibold flex text_underline relative max-sm:text-xl mb-4">
-              Analytics Tools
-            </h1>
-            <div className="w-full flex flex-wrap gap-4">
-              <SkillsFooter items={portfolioConfig.skills.analyticsTools} />
-            </div>
-          </FramerWrapper>
         </div>
       </div>
     </div>
